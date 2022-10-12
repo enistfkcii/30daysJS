23e54b32c7bdb61326316057d9ce39f36fb475c8
Enis Tufekci <enistfkcii@gmail.com>
2022-10-12 15:52:37 +0300

    30daysofJS de 1.GUN

---
+++ introduction.js
@@ -0,0 +1,16 @@
+console.log("bu yazı introduction.js den geldi")
+console.log(typeof true)
+console.log(typeof 1)
+console.log(typeof 1.7)
+console.log(typeof 'enis')
+console.log(typeof null)
+/* Degişken tanımlarken sadece $ ve _ özel ifadeleri kullanabiliriyoruz.
+ayrıca sayı ile başlayamıyoruz tanımlama yapmaya.*/
+let $name = "Naim Enis Tüfekçi",
+ _age = 25,
+ job = "Computer Engineering",
+ favFootballTeam = "Galatasaray",
+ married = false;
+const pi = 3.14
+
+console.log("İsmi:",$name,"Yaşı:",_age,"İşi:",job,"Takımı:",favFootballTeam,"Evli mi?:",married)
