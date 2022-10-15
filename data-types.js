eebdd2eec86ea14349bd63604c36143e7a89369d
Enis Tufekci <enistfkcii@gmail.com>
2022-10-15 15:07:07 +0300

    2.GUN DERSLERI EKLENDI

---
+++ data-types.js
@@ -0,0 +1,50 @@
+// let num1 = 3
+// let num2 = 3
+// console.log(num1 == num2);
+// let name1 = 'enis'
+// let name2 = 'naim'
+// console.log(name1 == name2)
+// let case1 = true ,case2 = false
+// console.log(case1 != case2)
+// let nums = [1,2,3,'enis',true]
+// nums[1] = 5
+// console.log(nums[3])
+// console.log(nums)
+// let user1 = {
+//     name : 'enis',
+//     surname : 'tufekci',
+//     age : 25,
+//     job : 'computer engineering',
+//     highschool : 'celal bayar university'
+// }
+// let user2 = {
+//     name : 'enis',
+//     surname : 'tufekci',
+//     age : 25,
+//     job : 'computer engineering',
+//     highschool : 'celal bayar university'
+// }
+// console.log(user1 == user2)
+// console.log(Math.max(1,4,6,2,8,5))
+// let number = Math.floor(Math.random() *11)
+// console.log(number)
+// let myData = "My name is Naim Enis Tüfekçi.\n I am 25 years old"
+// console.log(myData)
+// let a = 5;
+// let b = 4;
+// let fullName = `${a} + ${b} = ${a+b}` // çentikleri altgr+ctrl+"," ile yaptım 
+// console.log(fullName)
+// let n1 = 3
+// let n2 = 4
+// console.log(`${n1} ${n2} den daha küçük bir sayı. : ${n1}<${n2}`)
+let name = 'Naim Enis Tüfekçi'
+console.log(name.length)
+for(i=0;i<name.length-10;i++){
+    console.log(name[i])
+}
+console.log(name.indexOf('m'))
+let num = '5'
+let num1 = parseInt(num)
+console.log(num1)
+
+ 
---
+++ day2exercise.js
@@ -0,0 +1,22 @@
+let challenge = '30 Days of JS'
+console.log(`${challenge}`)
+console.log(`${challenge.length}`)
+console.log(`${challenge.toUpperCase()}`)
+console.log(`${challenge.toLowerCase()}`)
+console.log(`${challenge.substring(0,2)}`)
+console.log(`${challenge.includes('script')}`)
+let dizi = challenge.split(' ')
+console.log(dizi)
+let socialMedia = 'Facebook,Twitter,İnstagram,Linkedin'
+console.log(`${socialMedia.split(',')}`)
+console.log(`${socialMedia.replace('Facebook','tinder')}`)
+console.log(`${socialMedia.charAt(15)}`)
+console.log(`${socialMedia.indexOf('c')}`)
+let num1 = 5,num2 = '5'
+console.log(num1 == num2)
+console.log(Math.floor(Math.random() *110))
+console.log(Math.floor(Math.random(0.5,1) *110))
+let randomNumber = Math.random(0,challenge.length)
+console.log(Math.floor(randomNumber*10))
+console.log("1 1 1 1 1\n2 1 2 4 8\n3 1 3 9 27\n4 1 4 16 64\n5 1 5 25 125")
+let data = 'love is love my langueage love'
