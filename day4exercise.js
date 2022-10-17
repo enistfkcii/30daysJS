26f98b138ebd1dd691629732503d49ed557f1b22
Enis Tufekci <enistfkcii@gmail.com>
2022-10-17 17:57:05 +0300

    4.GUN DERSLERI EKLENDI

---
+++ day4exercise.js
@@ -0,0 +1,64 @@
+// let age = prompt("Yaş girin")
+// age >= 18 ? console.log("Ehliyet alabilirsin") : console.log(`Ehliyet almak için ${18-age} yil daha beklemelisin `)
+// let myAge = 25
+// let yourAge = prompt("Senin yaşın kaç?")
+// if(myAge > yourAge){
+//     console.log(`Sen benden ${myAge-yourAge} yaş küçüksün`)
+// }
+// else if(myAge == yourAge){
+//     console.log(`İkimizde aynı yaştayız yani ${myAge} yaşındayız`)
+// }
+// else{
+//     console.log(`Sen benden ${yourAge-myAge} yaş büyüksün`)
+// }
+// let number = prompt("Sayi gir")
+// switch(number%2){
+//     case 0 :
+//         console.log("Bu sayi çift sayıdır")
+//         break
+//     case 1 :
+//         console.log("Bu sayi tek sayıdır")
+//         break
+// }
+// let exam = prompt("Not giriniz")
+// let gradueteNote
+// if(exam >=90 && exam <= 100 ){
+//     gradueteNote = 'A'
+//     console.log(gradueteNote)
+// }
+// else if(exam >=70 && exam <= 89 ){
+//     gradueteNote = 'B'
+//     console.log(gradueteNote)
+// }
+// else if(exam >=50 && exam <= 69 ){
+//     gradueteNote = 'C'
+//     console.log(gradueteNote)
+// }
+// else if(exam <= 49 ){
+//     gradueteNote = 'F'
+//     console.log(gradueteNote)
+// }
+let seazon = prompt("Gün giriniz")
+switch(seazon){
+    case 'Pazartesi' :
+        console.log("Pazartesi günündeyiz")
+        break
+        case 'Salı' :
+            console.log("Salı günündeyiz")
+            break
+            case 'Çarşamba' :
+        console.log("Çarşamba günündeyiz")
+        break
+        case 'Perşembe' :
+        console.log("Perşembe günündeyiz")
+        break
+        case 'Cuma' :
+        console.log("Cuma günündeyiz")
+        break
+        case 'Cumartesi' :
+        console.log("Cumartesi günündeyiz")
+        break   
+        case 'Pazar' :
+        console.log("Pazar günündeyiz")
+        break   
+}
--- introduction.js
+++ introduction.js
@@ -14,13 +14,40 @@
 // const pi = 3.14
 
 // console.log("İsmi:",$name,"Yaşı:",_age,"İşi:",job,"Takımı:",favFootballTeam,"Evli mi?:",married)
-const case1 = 4<3 || 2>1
-const case2 = 7<1 && 1>0
-console.log(case1)
-console.log(case1&&case2)
-console.log(case1||case2)
-let yagmurluHava = false
-yagmurluHava ? console.log("Hava bugün yağmurlu") :
-console.log("Bugün hava açık")
-const date = new Date()
-console.log(date)
+// const case1 = 4<3 || 2>1
+// const case2 = 7<1 && 1>0
+// console.log(case1)
+// console.log(case1&&case2)
+// console.log(case1||case2)
+// let yagmurluHava = false
+// yagmurluHava ? console.log("Hava bugün yağmurlu") :
+// console.log("Bugün hava açık")
+// const date = new Date()
+// console.log(date)
+
+// let value = prompt("Bir deger giriniz")
+// if(value > 5) {
+//     console.log("Bu sayı 5 den büyük")
+// }
+// else if(value <5 ){
+//     console.log("Bu sayı 5 den küçük")
+// }
+// else{
+//     console.log("Bu sayı 5")
+// }
+// let age = prompt("Yaş giriniz")
+// switch(age){
+//     case '10' :
+//         console.log("Girilen sayı 10")
+//         break
+//     case '12' :
+//         console.log("Girilen sayı 12")
+//         break
+//     case '15' :
+//         console.log("Girilen sayı 15")
+//         break
+//     default :
+//         console.log("Girilen sayı tanınmıyor")
+// }
+let value = prompt("İsim giriniz")
+value.length >= 5 ? console.log("Geçerli bir isim") : console.log("En az 5 karakterden oluşan bir isim giriniz")
